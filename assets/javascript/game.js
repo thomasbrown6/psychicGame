f9f7463c1e6ee44679599118f36585755919cd07
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-06-03 18:01:32 -0400

    finished psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+ // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+ var computerChoices = [ "a", "b", "c", "d", "e", "f", "g",
+ "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+ "u", "v", "w", "x", "y", "z"];
+
+ var wins = 0;
+ var losses = 0;
+ var guessesLeft = 11;
+ var choices = [];
+
+ // This function is run whenever the user presses a key.
+ document.onkeyup = function(event) {
+
+   // Determines which key was pressed.
+   var userGuess = event.key;
+
+   // Randomly chooses a choice from the options array. This is the Computer's guess.
+   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+    if (userGuess === computerGuess) {
+       wins++;
+       guessesLeft = 10;
+       choices = [];
+  } else if (guessesLeft > 0) {
+       guessesLeft--;
+       var x = choices.push(userGuess);
+  } else {
+        losses++;
+        guessesLeft = 10;
+        choices = [];
+    }
+
+
+       var html =
+       "<p>Wins: " + wins + "</p>" +
+       "<p>Losses: " + losses + "</p>" +
+       "<p>Guesses Left: " + guessesLeft + "</p>" +
+       "<p>Your Guesses So Far: " + choices + "</p>" ;
+       
+
+    // Select the div with the id "game", and insert the following HTML into it.
+     document.querySelector("#game").innerHTML = html;
+
+ } 
+ 
+ 
+
+ 
