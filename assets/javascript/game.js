5640d033a9daed6fa6c983c10a1ef3550e0e117d
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-06-04 18:22:21 -0400

    updated with comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@
     "u", "v", "w", "x", "y", "z" 
     ];
 
+ // ***VARIABLES
  var wins = 0;
  var losses = 0;
  var guessesLeft = 10;
@@ -20,20 +21,27 @@
    // Randomly chooses a choice from the options array. This is the Computer's guess.
    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+   // If statement so that the user can't enter a value outside of the choices
+   // The application won't run unless user enters a valid entry
    if (computerChoices.includes(userGuess)) {
 
-    
+    // If statement so the user won't guess the same value twice
     if (choices.indexOf(userGuess) === -1) {
 
+     // What to do when the user guesses right
      if (userGuess === computerGuess) {
          wins++;
          guessesLeft = 10;
          choices = [];
 
+      // As long as the user has guesses left, subtract the gueses when the user guesses
+      // wrong and push the user guess to the choices array
     } else if (guessesLeft > 1) {
        guessesLeft--;
        choices.push(userGuess);
 
+      // If the user runs out of guesses then they lose and the guessesLeft and choices array 
+      // will reset.
     } else {
         losses++;
         guessesLeft = 10;
@@ -44,7 +52,7 @@
    }
 
     
-
+    // html to be inserted in index.html file
        var html =
        "<p>Wins: " + wins + "</p>" +
        "<p>Losses: " + losses + "</p>" +
